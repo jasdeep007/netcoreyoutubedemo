04be938b1cea902a788e501a25d5d3c36f2abd58
jasdeep singh <jasdeep.chauhan@hotmail.com>
2021-06-11 15:01:09 +0530

    part 4 angular - Middlewares

--- Startup.cs
+++ Startup.cs
@@ -25,7 +25,13 @@ namespace youtubedemonetcore
         public void ConfigureServices(IServiceCollection services)
         {
         }
-
+        private static void executethiscode(IApplicationBuilder app)
+        {
+            app.Run(async context =>
+            {
+                await context.Response.WriteAsync("Write code in response because /outtm was requested");
+            });
+        }
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
@@ -33,15 +39,51 @@ namespace youtubedemonetcore
             {
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // don't worry we will study it,,
+                // its just example that we can use custom page for exception
+                // because of this middleware
+            }
 
-            app.UseRouting();
+            ////// lets try to make simple middleware by use method            
+            ////// use method allows current middleware to be executed and also next code
+            ////app.Use(async (context, next) =>
+            ////{
+            ////    await context.Response.WriteAsync("(Request) I am from middleware by run method 1 \n");
+            ////    await next.Invoke();
+            ////    await context.Response.WriteAsync("(Response) I am from middleware by run method 1 \n");
+            ////});
+            ////// lets make another middleware with use
+            ////app.Use(async (context, next) =>
+            ////{
+            ////    await context.Response.WriteAsync("(Request) I am from middleware by run method 2 \n");
+            ////    await next.Invoke();
+            ////    await context.Response.WriteAsync("(Response) I am from middleware by run method 2 \n");
+
+            ////});
+            //////lets make another but this time by run method
+            ////app.Run(async (context) =>
+            ////{
+            ////    await context.Response.WriteAsync("(Request) I am from middleware by run method 3 \n");
+            ////    await context.Response.WriteAsync("(Response) I am from middleware by run method 3 \n");
+            ////});
+
+            //app.MapWhen(context => {
+            //    return context.Request.Query.ContainsKey("somekey"); // when query string contains key then execute below delegate
+            //}, executethiscode);
+            app.Map("/outtm", executethiscode);
+            
+
+            app.UseRouting(); // for routing 
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGet("/", async context =>
                 {
                     await context.Response.WriteAsync(
-                        config["mykey"].ToString()
+                        "I am after middleware code"
                         );
                 });
             });
