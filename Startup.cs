edab48400dd875b0320c69cde1067bb2dddab4db
jasdeep singh <jasdeep.chauhan@hotmail.com>
2021-06-15 12:19:40 +0530

    PART 7 - Environment Variables

--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,10 @@ namespace youtubedemonetcore
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            if (env.IsDevelopment() ||
+                env.IsProduction() ||
+                env.IsStaging() ||
+                env.IsEnvironment("UAT"))
             {
                 app.UseDeveloperExceptionPage();
             }
@@ -41,38 +44,9 @@ namespace youtubedemonetcore
                 app.UseExceptionHandler("/Error");
             }
 
-            //app.UseRouting();
 
-            //app.UseEndpoints(endpoints =>
-            //{
-            //    endpoints.MapGet("/", async context =>
-            //    {
-            //        await context.Response.WriteAsync(
-            //           "Hello World !!"
-            //            );
-            //    });
-            //});
-
-            //// this class helps us to override default options
-            //DefaultFilesOptions df = new DefaultFilesOptions();
-            //df.DefaultFileNames.Clear();// clear all default ones...
-            //df.DefaultFileNames.Add("foo.html");// now foo.html is the only default file
-
-            //app.UseDefaultFiles(df); // thats it,,,lets see the output now,,
-            //app.UseStaticFiles();
-
-            //// and then 
-            //app.UseDirectoryBrowser();
-
-            app.UseFileServer(enableDirectoryBrowsing:true);// by default directory browsing is not enabled in this
-
-            app.UseStaticFiles(new StaticFileOptions
-            {
-                FileProvider = new PhysicalFileProvider(
-                    Path.Combine(env.ContentRootPath, "staticfiles")
-                    ),
-                RequestPath = "/StaticFiles"               
-                
+            app.Run(async (context) => {
+                await context.Response.WriteAsync($"Current Environment is : {env.EnvironmentName}");
             });
 
         }
