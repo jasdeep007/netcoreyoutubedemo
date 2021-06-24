f1f6a2cd8c4af69c0ed6c06499886072423a0dd9
jasdeep singh <jasdeep.chauhan@hotmail.com>
2021-06-24 14:28:47 +0530

    Part  11- api and tasks in .netcore

---
+++ Controllers/apiv1Controller.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using youtubedemonetcore.models;
+
+namespace youtubedemonetcore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class apiv1Controller : ControllerBase
+    {
+        private readonly IEmployee emp;
+
+        //lets import IEmployee by dependency injection
+        public apiv1Controller(IEmployee emp)
+        {
+            this.emp = emp;
+        }
+        //lets create get api
+        [HttpGet]
+        [Route("[action]")]
+        public IActionResult get()
+        {
+            // lets analyse calculation time also
+            var starttime = DateTime.Now.ToString();
+            var d1 = getEmployee();
+            var d11 = getEmployee1();
+            var d111 = getEmployee11();
+            var d1111 = getEmployee111();
+            var d11111 = getEmployee1111();
+            var endtime = DateTime.Now.ToString();
+            var data = new
+            {
+                starttime=starttime,
+                endtime=endtime,
+                d1 = d1,
+                d11 = d11,
+                d111 = d111,
+                d1111 = d1111,
+                d11111 = d11111,
+            };
+            return Ok(data);
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public async Task<IActionResult> gettasks()
+        {
+            // lets analyse calculation time also
+            var starttime = DateTime.Now.ToString();
+            var d1 = Task.Run(()=>getEmployee()); // simple... code
+            var d11 = Task.Run(() => getEmployee1());
+            var d111 = Task.Run(() => getEmployee11());
+            var d1111 = Task.Run(() => getEmployee111());
+            var d11111 = Task.Run(() => getEmployee1111());
+
+            // now we will wait for them all to execute
+            await Task.WhenAll(d1, d11, d111);
+            //now we are sure that all done here
+            var endtime = DateTime.Now.ToString();
+            // let see now,,, // expecting 30 seconds or not ??????
+            //lets see
+            var data = new
+            {
+                starttime = starttime,
+                endtime = endtime,
+                d1 = d1,
+                d11 = d11,
+                d111 = d111,
+                d1111 = d1111,
+                d11111 = d11111,
+            };
+            return Ok(data);
+        }
+
+        [NonAction]
+        public List<Employee> getEmployee()
+        {
+            Thread.Sleep(10000);//10 seconds
+            return emp.GetEmployee();
+        }
+        [NonAction]
+        public List<Employee> getEmployee1()
+        {
+            Thread.Sleep(10000);//10 seconds
+            return emp.GetEmployee();
+        }
+        [NonAction]
+        public List<Employee> getEmployee11()
+        {
+            Thread.Sleep(10000);//10 seconds
+            return emp.GetEmployee();
+        }
+        [NonAction]
+        public List<Employee> getEmployee111()
+        {
+            Thread.Sleep(10000);//10 seconds
+            return emp.GetEmployee();
+        }
+        [NonAction]
+        public List<Employee> getEmployee1111()
+        {
+            Thread.Sleep(10000);//10 seconds
+            return emp.GetEmployee();
+        }
+    }
+}
---
+++ Controllers/v1Controller.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using youtubedemonetcore.models;
+
+namespace youtubedemonetcore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class v1Controller : ControllerBase
+    {
+        private readonly IEmployee emp;
+
+        public v1Controller(IEmployee emp)
+        {
+            this.emp = emp;
+        }
+
+        [HttpGet]
+        [Route("[action]/{i}")]
+        public async Task<IActionResult> get(int i)
+        {
+            var startTime = DateTime.Now.ToString();
+            var t1 = Task.Run(() => getdata1());
+            var t11 = Task.Run(() => getdata11(i));
+            var t111 = Task.Run(() => getdata111(i));
+
+            await Task.WhenAll(t1, t11, t111);
+            var endTime = DateTime.Now.ToString();
+            var data = new
+            {
+                startTime = startTime,
+                endtime = endTime,
+                ivalue = i,
+                d1 = t1.Status == TaskStatus.RanToCompletion ? t1.Result : null,
+                d2 = t1.Status == TaskStatus.RanToCompletion ? t1.Result : null,
+                d3 = t1.Status == TaskStatus.RanToCompletion ? t1.Result : null,
+            };
+
+            return Ok(data);
+
+        }
+        [HttpGet]
+        [Route("[action]/{i}")]
+        public IActionResult get1(int i)
+        {
+            var startTime = DateTime.Now.ToString();
+            var d1 = getdata1();
+            var d2 = getdata11(i);
+            var d3 = getdata111(i);
+            var endTime = DateTime.Now.ToString();
+            var data = new
+            {
+                startTime = startTime,
+                endtime = endTime,
+                ivalue = i,
+                d1 = d1,
+                d2 = d2,
+                d3 = d3,
+            };
+
+            return Ok(data);
+
+        }
+        [NonAction]
+        public object getdata1()
+        {
+            return new { threadID = Thread.CurrentThread.ManagedThreadId.ToString(), data = emp.GetEmployee() };
+        }
+        [NonAction]
+        public object getdata11(int i)
+        {
+            if (i == 1) Thread.Sleep(10000);
+            return new { threadID = Thread.CurrentThread.ManagedThreadId.ToString(), data = emp.GetEmployee() };
+        }
+        [NonAction]
+        public object getdata111(int ii)
+        {
+            if (ii == 1) Thread.Sleep(10000);
+            
+            return new { threadID = Thread.CurrentThread.ManagedThreadId.ToString(), data = emp.GetEmployee() };
+        }
+
+    }
+}
